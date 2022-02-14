2986b740e1d30d947825e5833754784d7b6f141d
SharvilPande <73053837+SharvilPande@users.noreply.github.com>
2022-02-14 18:54:26 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song_peter = "";
 song_potter = "";
+right_WristX = 0;
+right_WristY = 0;
+left_WristX = 0;
+leftWristY = 0;
 
 function preload() {
     song_peter = loadSound("Peter_pan.mp3");
@@ -11,8 +15,29 @@ function setup() {
     canvas.position(600, 250);
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
 }
+
+function modelLoaded() {
+    console.log("Posenet model is initialized");
+}
+
+function gotPoses(results) {
+    
+        if (results.length > 0) {
+            console.log(results);
+            right_WristX = results[0].pose.rightWrist.x;
+            right_WristY = results[0].pose.rightWrist.y;
+            console.log("right_WristX = " + right_WristX + ", right_WristY = " + right_WristY);
+            left_WristX = results[0].pose.leftWrist.x;
+            left_WristY = results[0].pose.leftWrist.y;
+            console.log("left_WristX = " + left_WristX + ", left_WristY = " + left_WristY);
+        }
+    }
+
 
 function draw() {
     image(video, 0, 0, 350, 350);
 }
+
